de6bf27050141824ca251ba604861baa68fe42fc
Pedro <pedro.alves@escs.edu.br>
2025-10-05 17:03:21 -0300

    feat(schedules): implementar repositório de Schedules

--- src/repositories/SchedulesRepository.js
+++ src/repositories/SchedulesRepository.js
@@ -1,5 +1,25 @@
-class SchedulesRepository {
+const SchedulesModel = require('../models/SchedulesModel');
 
+class SchedulesRepository {
+    static async findById(id_schedule) {
+        return SchedulesModel.findById(id_schedule);
+    }
+
+    static async findAll() {
+        return SchedulesModel.findAll();
+    }
+
+    static async create(scheduleData) {
+        return SchedulesModel.create(scheduleData);
+    }
+
+    static async update(id_schedule, scheduleData) {
+        return SchedulesModel.update(id_schedule, scheduleData)
+    }
+
+    static async delete(id_schedule) {
+        return SchedulesModel.delete(id_schedule);
+    }
 }
 
 module.exports = SchedulesRepository;
