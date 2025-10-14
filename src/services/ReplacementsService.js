4dd92f3bb06a39dfa68d2f6cb579ab18b8eddd1b
Pedro <pedro.alves@escs.edu.br>
2025-10-13 23:35:01 -0300

    feat: Implementação completa do módulo de Replacements (Reposições)

---
+++ src/services/ReplacementsService.js
@@ -0,0 +1,26 @@
+const ReplacementsRepository = require('../models/ReplacementsModel');
+
+class ReplacementService {
+    
+    static async findById(id_replacement) {
+        return ReplacementsRepository.findById(id_replacement);
+    }
+
+    static async findAll() {
+        return ReplacementsRepository.findAll();
+    }
+
+    static async create(replacementData) {
+        return ReplacementsRepository.create(replacementData);
+    }
+
+    static async update(id_replacement, replacementData) {
+        return ReplacementsRepository.update(id_replacement, replacementData);
+    }
+
+    static async delete(id_replacement) {
+        return ReplacementsRepository.delete(id_replacement);
+    }
+}
+
+module.exports = ReplacementService;
